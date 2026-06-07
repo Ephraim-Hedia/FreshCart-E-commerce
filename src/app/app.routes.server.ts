a87abf17ab9bc9b2bc4226976df42325bebc649f
Guirguis <ephraimhedia2@gmail.com>
2026-06-07 17:02:28 +0300

    Add new product route for client-side rendering. Introduce a route for product details with dynamic parameters, enhancing navigation and user experience.

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,12 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'products/:id/:slug',
+    renderMode: RenderMode.Client,
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
+
 ];
